7b7576cb7c7c39569474dc62442d3357c4adb78b
jean <jean.leal22@gmail.com>
2020-02-09 19:29:11 -0300

    return type for xml

--- src/main/java/com/jean/mobileappws/controller/UserController.java
+++ src/main/java/com/jean/mobileappws/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.jean.mobileappws.controller;
 
+import com.jean.mobileappws.model.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,8 +11,22 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping
-    public String getUser() {
-        return "get user was called";
+    public String getUsers(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "limit", defaultValue = "50") int limit,
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort
+    ) {
+        return "get users was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
+    }
+
+    @GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> getUser(@PathVariable Integer userId) {
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail("jean.leal22@gmail.com");
+        returnValue.setFirstName("Jean");
+        returnValue.setLastName("Leal");
+        returnValue.setUserId(userId);
+        return new ResponseEntity<>(returnValue, HttpStatus.OK);
     }
 
     @PostMapping
---
+++ src/main/java/com/jean/mobileappws/model/UserRest.java
@@ -0,0 +1,40 @@
+package com.jean.mobileappws.model;
+
+public class UserRest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer userId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
